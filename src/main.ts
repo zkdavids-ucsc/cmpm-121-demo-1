0fd6cdeef148d2ff4185a0725f0e67f0d5d9113b
Zeke Davidson <zkdavids@ucsc.edu>
2024-10-08 11:58:05 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,9 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const buttonText = "Click Here👹";
+
+const butt = document.createElement("button");
+butt.innerHTML = buttonText;
+app.append(butt);
