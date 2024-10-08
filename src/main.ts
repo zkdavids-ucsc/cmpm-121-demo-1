80b776afed699b7693176aa8a3056b50e850dffa
Zeke Davidson <zkdavids@ucsc.edu>
2024-10-08 11:41:34 -0700

    First Commit (title change)

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game";
+const gameName = "Game Title";
 document.title = gameName;
 
 const header = document.createElement("h1");
