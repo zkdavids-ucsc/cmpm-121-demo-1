c256214b56f5b987c21d048166a6ff86d55d3b9b
Zeke Davidson <zkdavids@ucsc.edu>
2024-10-09 16:33:06 -0700

    Step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ app.append(counterDiv);
 const incrementCounter = (): void => {
   count += 1;
   counterDiv.innerHTML = count.toFixed(2) + " demons";
+  upgrade.disabled = (count < 10);
 };
 
 button.addEventListener("click", incrementCounter);
@@ -30,13 +31,26 @@ button.addEventListener("click", incrementCounter);
 // setInterval(incrementCounter, 1000);
 
 let zero = performance.now();
-function increment(){
-  // const value = (performance.now() - zero) / 1000;
-  // count += value;
-  const value = (performance.now() - zero) / 1000;
+let upgradeValue = 0;
+function increment() {
+  const value = (performance.now() - zero) / 1000 * upgradeValue;
   count += value;
   counterDiv.innerHTML = count.toFixed(2) + " demons";
   zero = performance.now();
+  upgrade.disabled = (count < 10);
 }
 requestAnimationFrame(increment);
 setInterval(increment, (performance.now() - zero) / 1000);
+
+const upgrade = document.createElement("button");
+upgrade.innerHTML = "Upgrade";
+app.append(upgrade);
+upgrade.disabled = (count < 10);
+
+const upgradeCounter = (): void => {
+  count -= 10;
+  upgradeValue += 1;
+  upgrade.disabled = (count < 10);
+};
+
+upgrade.addEventListener("click", upgradeCounter);
